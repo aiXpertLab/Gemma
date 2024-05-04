2ae7dd3300ae9ee038d425408d49aff14fd1647d
aiXpertLab <aiXpertLab@gmail.com>
2024-05-04 09:13:34 -0400

    Gemma

--- pages/🍨Gemma.py
+++ pages/🍨Gemma.py
@@ -1,10 +1,13 @@
-# from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import os
 from transformers import AutoTokenizer, AutoModelForCausalLM
+token = os.environ.get("HF_TOKEN")
 
-model_name = "google/gemma-2b-it"
+model_name = "google/gemma-1.1-2b-it"
+# model_name = "E:/models/2b-gemma"     # if running local computer
+
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 # model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-model=AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto")
+model=AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", token=token)
 
 input_text = "write me a poem about beautifuly woman."
 input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
